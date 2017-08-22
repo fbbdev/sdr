6bf904d67544d325675b1cd5a5b986ed59c222b3
Fabio Massaioli <fabio.massaioli@gmail.com>
2017-08-22 23:18:23 +0200

    Import kfr

---
+++ signal.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stream/stream.hpp"
+
+#include <cstdint>
+#include <complex>
+
+namespace sdr
+{
+
+using RealSample = float;
+using Sample = std::complex<float>;
+
+} /* namespace sdr */
